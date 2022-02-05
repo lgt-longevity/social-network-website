d0e099ff6fa517cb59212ec54b9b12a0e0be8fd8
Fábio Maia <fabio.maia@itsector.pt>
2022-02-05 00:04:57 +0000

    feat: add postcss dependency

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,49 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>LGT - Longevity Social Network</h1>
-      Website in development...
-    </div>
+    <>
+      <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+          <span className="text-lg font-bold">With two icons</span>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+        <h1>LGT - Longevity Social Network</h1>
+        Website in development...
+      </div>
+    </>
   );
 }
